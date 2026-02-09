443a9ef5c0b0ff9781419a6ef06bccfce02dbb1c
Lelio <crisci.lelio04@gmail.com>
2026-02-09 11:24:40 +0100

    Refactor WelcomActivity

--- app/src/main/java/com/example/parkpin/WelcomeActivity.java
+++ app/src/main/java/com/example/parkpin/WelcomeActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
 import androidx.appcompat.app.AppCompatActivity;
 
